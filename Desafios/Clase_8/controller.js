00594de9cd28fac6630b5f2d16831045ec3eed9a
Jonathan Olbes <j.olbes.co@gmail.com>
2022-08-06 21:21:56 -0500

    Modificación entrega Clase_8

--- Desafios/Clase_8/controller.js
+++ Desafios/Clase_8/controller.js
@@ -3,32 +3,60 @@ class Producto {
     constructor(ruta){
         this.ruta = ruta
     }
+
+    async #readFileFunction(ruta){
+        let file = await fs.promises.readFile(ruta, 'utf-8')
+        let parseFile = await JSON.parse(file)
+        return parseFile
+    }
+
 //Agregar producto a un array ✅ 
     async save(objeto){
         try{
-            let dataArch = await fs.promises.readFile(this.ruta, 'utf-8') //Lee el archivo de la ruta especificada en formato texto
-            let dataArchParse = JSON.parse(dataArch) //Parse a JavaScript (JSON) el archivo en formato texto y lo guardamos en la variable
-            if (dataArchParse.length) { //Condicion 
+            let dataArch = await this.#readFileFunction(this.ruta, 'utf-8') //Lee el archivo de la ruta especificada en formato texto
+            if (dataArch.length) { //Condicion 
                 //Si dataArchParse.length != a cero(0) genera un nuevo objeto y "id" consecutivo del anterior
-                await fs.promises.writeFile(this.ruta, JSON.stringify([...dataArchParse, {...objeto, id: dataArchParse.length + 1} ], null, 2))
+                await fs.promises.writeFile(this.ruta, JSON.stringify([...dataArch, {...objeto, id: dataArch[dataArch.length - 1].id + 1} ], null, 2))
             } else {
                 //Si devuelve falso ó cero(0) dataArchParse.lenght => Crea un nuevo objeto
-                await fs.promises.writeFile(this.ruta, JSON.stringify([{...objeto, id: dataArchParse.length + 1}], null, 2))
+                await fs.promises.writeFile(this.ruta, JSON.stringify( [{...objeto, id: 1}], null, 2))
             }
             //Mostramos confirmación de Producto guardado por consola. Se muestra el ID generado.
-            return(`Producto guardado. Se identifica con el ID: ${dataArchParse.length + 1}`)
+            return(`Producto guardado. Se identifica con el ID: ${ dataArch[dataArch.length - 1].id + 1}`)
             //console.log(`Producto guardado. Se identifica con el ID: ${dataArchParse.length + 1}`)
-        }catch{
-            console.log('Algo salío mál (en .save) ')
+        }catch (error) {
+            console.log(`Algo salío mál (en .save) ${error}`)
         }
     
+    }
+
+//Actualizar producto por ID
+    async updateById(objeto) {
+        try {             
+            let dataArch = await this.#readFileFunction(this.ruta, 'utf-8') //Lee el archivo txt
+            const objIndex = dataArch.findIndex(producto => producto.id === objeto.id) // -1 o la posicion del objeto
+    console.log(objIndex)
+            if (objIndex !== -1) {
+                // Existe
+                dataArch[objIndex] = objeto
+                await fs.promises.writeFile(this.ruta, JSON.stringify( dataArch, null, 2))  
+                return {msg: 'Producto ha sido actualizado'}
+                //console.log(dataArch)
+            } else {
+                // no existe
+                return {error: 'El producto no existe'}
+            }           
+            
+        } catch (error) {
+            console.log(error)
+        }            
+       
     }
 //Mostrar producto por el ID ✅ 
     async getById(id){
         try {
-            let dataArch = await fs.promises.readFile(this.ruta, 'utf-8') //Lee el archivo txt
-            let dataArchParse = JSON.parse(dataArch) //Paasa a formato JavaScript / JSON
-            let producto = dataArchParse.find(producto => producto.id === id)
+            let dataArch = await this.#readFileFunction(this.ruta, 'utf-8') //Lee el archivo txt
+            let producto = dataArch.find(producto => producto.id === id)
             if (producto) {
                 return(producto) //Devuelve el producto con el ID solicitado
                 //console.log(producto)
@@ -39,30 +67,31 @@ class Producto {
             console.log(error)
         }
     }
+
+
 //Mostrar todos los productos ✅ 
         async getAll(){
             try {
-                let dataArch = await fs.promises.readFile(this.ruta, 'utf-8') //Lee el archivo txt
-                let dataArchParse = JSON.parse(dataArch) //Pasa a formato JavaScript / JSON
-                if (dataArchParse.length) { //Comprueba si no existe registros/productos en el archivo txt
-                    return(dataArchParse)
+                let dataArch = await this.#readFileFunction(this.ruta, 'utf-8') //Lee el archivo txt
+                if (dataArch.length) { //Comprueba si no existe registros/productos en el archivo txt
+                    return dataArch
                     //console.log(dataArchParse) //Muestra por consola los productos
                 } else {
-                    console.log('No hay productos registrados') //Si no existe datos en dataArchParse.lenght informa que no hay productos
+                    return null
                 }
             }catch (error){
                 console.log(error)
             }
-        }
+    }
+
 //Eliminar Producto por ID ✅ 
         async deleteById(id){
             try {
-                let dataArch = await fs.promises.readFile(this.ruta, 'utf-8')
-                let dataArchParse = JSON.parse(dataArch)
-                let producto = dataArchParse.find(producto => producto.id === id)
+                let dataArch = await this.#readFileFunction(this.ruta, 'utf-8')
+                let producto = dataArch.find(producto => producto.id === id)
                     if (producto) {
-                    let dataArchParseFilter = dataArchParse.filter(producto => producto.id !== id)
-                    await fs.promises.writeFile(this.ruta, JSON.stringify(dataArchParseFilter, null, 2))
+                    const dataArchParseFiltered = dataArch.filter(producto => producto.id !== id)
+                    await fs.promises.writeFile(this.ruta, JSON.stringify(dataArchParseFiltered, null, 2), 'utf-8')
                         return(`El producto con el ID: "${id}" ha sido eliminado`)
                         //console.log(`El producto con el ID: "${id}" ha sido eliminado`) 
                     } else {
@@ -71,24 +100,24 @@ class Producto {
             } catch (error) {
                 console.log(error)
             }
-        }
-//Borrar todos los productos ✅  
+    }
+
+    //Borrar todos los productos ✅  
         async deleteAll(){
             await fs.promises.writeFile(this.ruta, JSON.stringify([], null, 2), 'utf-8')
         }
 //Seleccionar Item aleatoriamente
         async randomItem(){
             try {
-                let dataArch = await fs.promises.readFile(this.ruta, 'utf-8') //Lee el archivo de la ruta especificada en formato texto
-                let dataArchParse = JSON.parse(dataArch) //Parse a JavaScript (JSON) el archivo en formato texto y lo guardamos en la variable
-                let randomItem = dataArchParse[Math.floor(Math.random() * dataArchParse.length)]
+                let dataArch = await this.#readFileFunction(this.ruta, 'utf-8') //Lee el archivo de la ruta especificada en formato texto
+                let randomItem = dataArch[Math.floor(Math.random() * dataArch.length)]
                 return(randomItem)
-                //console.log(randomItem)
+
             } catch (error) {
                 console.log(error)
             }
-        }
+    }
 
 }
 
-module.exports = Producto
+module.exports = { Producto }
--- Desafios/Clase_8/server.js
+++ Desafios/Clase_8/server.js
@@ -1,115 +1,73 @@
-const fs = require('fs')
-class Producto {
-    constructor(ruta){
-        this.ruta = ruta
+/* ******** IMPORT / REQUIRE ******** */
+const express = require ('express');
+const { Producto } = require('./controller');
+const producto = new Producto('./productos.txt')
+
+
+
+const app = express()
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended:true }) )
+
+
+/* ************  ************ */
+const { Router } = express //Destructuring de Express
+const routerProductos = Router() //Enrutado
+
+routerProductos.get('/', async (req, res) => {
+    try {
+        const respuesta = await producto.getAll()
+        res.json({
+            mensaje: 'Se muestran todos los productos',
+            respuesta
+        })
+    } catch (error) {
+        console.log(error)
     }
-//Agregar producto a un array ✅ 
-    async save(objeto){
-        try{
-            let dataArch = await fs.promises.readFile(this.ruta, 'utf-8') //Lee el archivo de la ruta especificada en formato texto
-            let dataArchParse = JSON.parse(dataArch) //Parse a JavaScript (JSON) el archivo en formato texto y lo guardamos en la variable
-            if (dataArchParse.length) { //Condicion 
-                //Si dataArchParse.length != a cero(0) genera un nuevo objeto y "id" consecutivo del anterior
-                await fs.promises.writeFile(this.ruta, JSON.stringify([...dataArchParse, {...objeto, id: dataArchParse.length + 1} ], null, 2))
-            } else {
-                //Si devuelve falso ó cero(0) dataArchParse.lenght => Crea un nuevo objeto
-                await fs.promises.writeFile(this.ruta, JSON.stringify([{...objeto, id: dataArchParse.length + 1}], null, 2))
-            }
-            //Mostramos confirmación de Producto guardado por consola. Se muestra el ID generado.
-            return(`Producto guardado. Se identifica con el ID: ${dataArchParse.length + 1}`)
-            //console.log(`Producto guardado. Se identifica con el ID: ${dataArchParse.length + 1}`)
-        }catch{
-            console.log('Algo salío mál (en .save) ')
-        }
-    
+
+})
+
+routerProductos.post('/', async (req, res) => {
+
+    try {
+        const objetoProducto = req.body
+        const respuesta = await producto.save(objetoProducto)
+        res.json({
+            mensaje: 'Producto guardado',
+            respuesta
+        })
+    } catch (error) {
+        console.log(error)
     }
-//Mostrar producto por el ID ✅ 
-    async getById(id){
-        try {
-            let dataArch = await fs.promises.readFile(this.ruta, 'utf-8') //Lee el archivo txt
-            let dataArchParse = JSON.parse(dataArch) //Paasa a formato JavaScript / JSON
-            let producto = dataArchParse.find(producto => producto.id === id)
-            if (producto) {
-                return(producto) //Devuelve el producto con el ID solicitado
-                //console.log(producto)
-            } else {
-                console.log(`El producto con el ID: ${id} no existe`)
-            }
-        } catch (error) {
-            console.log(error)
-        }
+
+})
+//routerProductos.get('/',)
+
+
+routerProductos.put('/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+        const objetoProducto = req.body
+        const respuesta = await producto.updateById({id: parseInt(id), ...objetoProducto})
+        res.json({mensaje: respuesta})
+    } catch (error) {
+        console.log(error)
     }
-//Mostrar todos los productos ✅ 
-        async getAll(){
-            try {
-                let dataArch = await fs.promises.readFile(this.ruta, 'utf-8') //Lee el archivo txt
-                let dataArchParse = JSON.parse(dataArch) //Pasa a formato JavaScript / JSON
-                if (dataArchParse.length) { //Comprueba si no existe registros/productos en el archivo txt
-                    return(dataArchParse)
-                    //console.log(dataArchParse) //Muestra por consola los productos
-                } else {
-                    console.log('No hay productos registrados') //Si no existe datos en dataArchParse.lenght informa que no hay productos
-                }
-            }catch (error){
-                console.log(error)
-            }
-        }
-//Eliminar Producto por ID ✅ 
-        async deleteById(id){
-            try {
-                let dataArch = await fs.promises.readFile(this.ruta, 'utf-8')
-                let dataArchParse = JSON.parse(dataArch)
-                let producto = dataArchParse.find(producto => producto.id === id)
-                    if (producto) {
-                    let dataArchParseFilter = dataArchParse.filter(producto => producto.id !== id)
-                    await fs.promises.writeFile(this.ruta, JSON.stringify(dataArchParseFilter, null, 2))
-                        return(`El producto con el ID: "${id}" ha sido eliminado`)
-                        //console.log(`El producto con el ID: "${id}" ha sido eliminado`) 
-                    } else {
-                        return(`No se ha encontrado el producto con el ID: ${id}`)
-                        //console.log(`No se ha encontrado el producto con el ID: ${id}`)
-                    }
-            } catch (error) {
-                console.log(error)
-            }
-        }
-//Borrar todos los productos ✅  
-        async deleteAll(){
-            await fs.promises.writeFile(this.ruta, JSON.stringify([], null, 2), 'utf-8')
-        }
-//Seleccionar Item aleatoriamente ✅
-        async randomItem(){
-            try {
-                let dataArch = await fs.promises.readFile(this.ruta, 'utf-8') //Lee el archivo de la ruta especificada en formato texto
-                let dataArchParse = JSON.parse(dataArch) //Parse a JavaScript (JSON) el archivo en formato texto y lo guardamos en la variable
-                let randomItem = dataArchParse[Math.floor(Math.random() * dataArchParse.length)]
-                return(randomItem)
-                //console.log(randomItem)
-            } catch (error) {
-                return(`Ups! hay un error: ${error}`)
-            }
-        }
 
-        async updateById(objeto){ 
-            try {
-                let dataArch = await fs.promises.readFile(this.ruta, 'utf-8')
-                let dataArchParse = JSON.parse(dataArch)
-                return dataArchParse
-                const objetoIndex = dataArchParse.findIndex(producto => producto.id === objeto.id)
+} )
+/* ************  ************ */
+app.use('/api/productos', routerProductos)
 
-                if (objetoIndex !== -1) {
-                    dataArchParse[objetoIndex] = objeto
-                    await fs.promises.writeFile(this.ruta, JSON.stringify(dataArchParse, null, 2))
-                    return{Mensaje: "El producto ha sido actualizado"}
-                    //console.log(first)
-                }else{
-                    return(`Ups! hay un error: ${error}`)
-                }
-            } catch (error) {
-                console.log(error)
-            }
-        }
 
-}
 
-module.exports = { Producto }
+
+/* ******** PORT CONFIG ******** */
+const PORT = 8080;
+
+const server = app.listen(PORT, () =>{
+    console.log(`Escuchando en el puerto: ${server.address().port} `)
+});
+
+server.on('Ha sucedido un error', error => console.log(error))
