2f6e4d35ae74c4c86fe4e717a6bdd564400be90a
Jonathan Olbes <j.olbes.co@gmail.com>
2022-08-18 09:20:53 -0500

    Creación de chat y muestra de productos en index

--- Desafios/Clase_12/server.js
+++ Desafios/Clase_12/server.js
@@ -1,12 +1,49 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
-const app = express();
+
 const { Contenedor } = require("./contenedor");
+const { Server: HttpServer } = require("http");
+const { Server: IoServer } = require("socket.io");
+
+const app = express();
+const httpServer = new HttpServer(app);
+const io = new IoServer(httpServer);
 
 app.use(express.urlencoded({ extended: true }));
 const port = process.env.PORT || 8080;
 
-const contenedor = new Contenedor("./productos.txt");
+const contenedor = new Contenedor("./productos.json");
+const comentarios = new Contenedor("./mensajes.json");
+
+io.on("connection", async socket => {
+	let mensajesChat = await comentarios.getAll();
+	console.log("Se contectó un usuario");
+
+	const mensaje = {
+		mensaje: "ok",
+		mensajesChat
+	};
+
+	socket.emit("mensaje-servidor", mensaje);
+
+	socket.on("mensaje-nuevo", async (msg, cb) => {
+		mensajesChat.push(msg);
+		const mensaje = {
+			mensaje: "mensaje nuevo",
+			mensajesChat
+		};
+
+		const id = new Date().getTime();
+		io.sockets.emit("mensaje-servidor", mensaje);
+		cb(id);
+		await comentarios.save({
+			id,
+			mail: msg.mail,
+			mensaje: msg.mensaje,
+			fecha: msg.hora
+		});
+	});
+});
 
 app.set("view engine", "hbs");
 app.set("views", "./views/layouts");
@@ -32,7 +69,7 @@ app.get("/", async (req, res) => {
 	});
 });
 
-app.get("/productos", async (req, res) => {
+app.get("/", async (req, res) => {
 	const producto = await contenedor.getAll();
 	res.render("productos", {
 		titulo: "Redes sociales",
@@ -42,14 +79,13 @@ app.get("/productos", async (req, res) => {
 	});
 });
 
-app.post("/productos", async (req, res) => {
+app.post("/", async (req, res) => {
 	const objProducto = req.body;
 	contenedor.save(objProducto);
-	const listExist = true;
-	res.redirect("/productos");
+	res.redirect("/");
 });
 
-app.listen(port, err => {
+httpServer.listen(port, err => {
 	if (err) throw new Error(`Error al iniciar el servidor: ${err}`);
 	console.log(`Server is running on port ${port}`);
 });
